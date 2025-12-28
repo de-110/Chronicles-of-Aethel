e0cf2ba8a98af7faa0e7abc17e0c0ef8ce673d38
de-110 <183182702+de-110@users.noreply.github.com>
2025-12-28 11:39:36 +0800

    add simple enemy movement and tile layers

--- scene/slime.gd
+++ scene/slime.gd
@@ -2,10 +2,18 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 @export var target:Node2D
-
-
+var player_detected: bool = false
 
 func _physics_process(delta: float) -> void:
-	var direction = (target.global_position - global_position).normalized()
-	velocity = direction * SPEED
-	move_and_slide()
+	if player_detected:
+		var direction = (target.global_position - global_position).normalized()
+		velocity = direction * SPEED
+		move_and_slide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player_detected = true
+	print("fouunddd youuuu")
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player_detected = false
+	print("wer u at nigga")
